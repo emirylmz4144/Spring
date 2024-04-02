20bf9ffa5e4a072dc8bf50f40a96fe0fc30f917e
emirylmz4144 <emiryilmaz4144@gmail.com>
2024-04-02 18:58:36 +0300

    comment line updated

--- DependencyInjections/src/ExampleOf_Depend_DependInject/step02_BetterHelloWorld/Application.java
+++ DependencyInjections/src/ExampleOf_Depend_DependInject/step02_BetterHelloWorld/Application.java
@@ -2,8 +2,10 @@ package ExampleOf_Depend_DependInject.step02_BetterHelloWorld;
 
 public class Application {
 	/**
-	 *Biz webSocket'e web'e yazdırmak isteriz yani şu an mesaja bağımlılık noktasında daha iyi noktadayız ama hem konsola bağımlı hem de
-	 *  mesajı alıyor ve yazdırıyor yani iki işi birden yapıyor
+	 *Mesajı alıyor ve yazdırıyor yani iki işi birden yapıyor
+	 *yani şu an mesaja bağımlılık noktasında daha iyi noktadayız ama hem konsola bağımlı hem de
+	 * Biz webSocket'e web'e yazdırmak isteriz ama  bunu yapamaz
+	 *
 	 * --> step03
 	 */
 
