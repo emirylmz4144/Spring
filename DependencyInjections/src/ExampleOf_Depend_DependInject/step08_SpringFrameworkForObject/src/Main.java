46c8ba517e6bf770b9f327da3254be5d1f7e9e53
emirylmz4144 <emiryilmaz4144@gmail.com>
2024-04-09 14:49:58 +0300

    comment line added

--- DependencyInjections/src/ExampleOf_Depend_DependInject/step08_SpringFrameworkForObject/src/Main.java
+++ DependencyInjections/src/ExampleOf_Depend_DependInject/step08_SpringFrameworkForObject/src/Main.java
@@ -2,7 +2,15 @@ import GreetingRendererTypes.IGreetingRenderer;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
+/**
+ * Böylece hem objeleri oluşturup hem bağımlılıkları yönettik ve bunu gerçek anlamda spring
+ * ile yapmış olduk
+ * Yani Spring
+ *       -Dünyadaki en büyük Factory
+ *       -Dünyadaki en büyük DI yönetimi
+ *       -Objeleri oluşturup dependencyleri yönetmenin en temel yoludur 
+ *
+ */
 public class Main {
     public static void main(String[] args) {
         BeanFactory factory=new ClassPathXmlApplicationContext("resources/beans.xml");
